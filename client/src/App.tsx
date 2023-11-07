3d30c5ffe20829ac037d7d903b55d93096b8c518
Oleh <olegsuv.ukr@gmail.com>
2023-11-07 21:21:09 +0200

    mod:  frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import "./css/App.css";
+import PostDescription from "./components/NewPost";
 
 function App() {
-  return <big>What's up bitches. it's start config</big>;
+  return (
+    <div className="grid grid-cols-3 gap-4 ">
+      <div className="bg-slate-500 collapse">
+        what's up dev. it's left column
+      </div>
+      <div className="col-span-full md:col-span-1">
+        <PostDescription />
+      </div>
+      <div className="bg-slate-700 collapse">
+        what's up dev. it's right column
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/NewPost.tsx
@@ -0,0 +1,24 @@
+export default function PostDescription() {
+  return (
+    <div className="p-4 flex flex-col justify-center items-center h-screen">
+      <input
+        type="text"
+        id="title"
+        placeholder="Default title"
+        className="block w-full p-2.5 text-xl text-gray-900 sm:text-md dark:bg-slate-800 dark:text-white !outline-none font-semibold"
+      ></input>
+      <textarea
+        id="description"
+        rows={5}
+        className="block resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white !outline-none"
+        placeholder="Your minds..."
+      ></textarea>
+      <button
+        type="button"
+        className="text-white font-semibold bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br  shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 rounded-lg text-sm px-5 py-2.5 text-center mt-2 mr-0 ml-auto"
+      >
+        Create new post
+      </button>
+    </div>
+  );
+}
