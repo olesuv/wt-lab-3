7a09fc446ef03130c7f4e6573443527ea8b89c5d
Oleh <olegsuv.ukr@gmail.com>
2023-11-15 22:46:31 +0200

    add: defaults for model

--- server/models/post.js
+++ server/models/post.js
@@ -2,9 +2,19 @@ import mongoose from "mongoose";
 const { model, Schema, maxLength } = mongoose;
 
 var postSchema = new Schema({
-  title: { type: String, required: true, maxLength: 50 },
-  description: { type: String, required: true, maxLength: 250 },
-  author: { type: String, required: true, maxLength: 50 },
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+    default: "Default title",
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 250,
+    default: "My minds...",
+  },
+  author: { type: String, required: true, maxLength: 50, default: "John Doe" },
   uploadDate: { type: Date, default: Date.now },
 });
 
